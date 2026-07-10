dcfd05ab779ff8ef0b4d712ff8862be7f2ef2cdb
69011344 <69011344@kmitl.ac.th>
2026-07-10 05:29:22 +0000

    complete lab1

---
+++ wk1-2/federico.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main () {
+    printf("69011344 Federico.\n\n");
+    printf("%s\n", "KMITL");
+    printf("%d\n", 100);
+    printf("%f\n", 4.55);
+    printf("%c\n", 'A');
+    printf("%s %d %f %c\n","POOM", 100,4.55555, 'A');
+
+    return(0);
+
+}
---
+++ wk1-2/federico2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main (){
+
+  printf("Hello World! \n");
+  printf("This is my first Program");
+  printf("\n\t I'm an engineer");
+
+  return (0);
+
+}
